231c9c93153a7d8a10981e95361d558a3d297984
Jac Lemieux <jac@Jacs-MacBook-Air.local>
2024-09-08 11:02:20 -0700

    initial push

---
+++ src/gen_ai_blocks/completion.py
@@ -0,0 +1,151 @@
+from typing import List, Dict, Type
+from openai import OpenAI
+
+from typing import Optional, List, Dict
+
+from openai._base_client import SyncAPIClient
+from pydantic import BaseModel, Field
+import re
+
+
+class Parameter(BaseModel):
+    type: str
+    name: str
+    description: str
+    required: bool = Field(default=True)
+    enum: Optional[List[str]] = None
+
+
+class Tool(BaseModel):
+    name: str
+    description: str
+    parameters: List[Parameter]
+
+
+    def dump(self) -> Dict:
+        out = {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "parameters": {
+                    "type": "object",
+                    "properties": {}
+                },
+            }
+        }
+        for parameter in self.parameters:
+            required = []
+            chunk = {
+                "type": parameter.type,
+                "description": parameter.description
+            }
+            if parameter.type == "array":
+                chunk["items"] = {"type": "string"}
+
+            if parameter.enum is not None:
+                chunk["enum"] = parameter.enum
+
+            out["function"]["parameters"]["properties"][parameter.name] = chunk
+
+            if parameter.required is True:
+                required.append(parameter.name)
+
+        return out
+
+
+class Completion:
+    def complete(self, messages: List[Dict[str, str]], tools: List[Tool] = None) -> str:
+        pass
+
+    def parse(self, messages: List[Dict[str, str]], response_format: Type[BaseModel] = None) -> str:
+        pass
+
+    def force_tool(self, messages: List[Dict[str, str]], tools: List[Tool]) -> List:
+        pass
+
+    def _compress_messages(self, messages):
+        # TODO: summarize if length exceeds treshold
+        compressed_messages: List[Dict[str, str]] = []
+        for msg in messages:
+            compressed_content = re.sub(r'\s+', ' ', msg["content"]).strip()
+            compressed_messages.append({"role": msg["role"], "content": compressed_content})
+        return compressed_messages
+
+
+class OpenAICompletion(Completion):
+    client: SyncAPIClient
+    parse_model: str
+    completion_model: str
+    tool_model: str
+    default_model: str
+    default_temperature: str
+
+    def __init__(self,
+                 completion_model=None,
+                 parse_model=None,
+                 tool_model=None,
+                 default_model=None,
+                 default_temperature=None
+                 ):
+        self.default_model = default_model or "gpt-4o"
+        self.completion_model = completion_model or self.default_model
+        self.parse_model = parse_model or "gpt-4o-2024-08-06"
+        self.tool_model = tool_model or self.default_model
+        self.default_temperature = default_temperature or 0
+        self.client = OpenAI()
+
+    def parse(self, messages: List[Dict[str, str]], response_format: Type[BaseModel] = None, ) -> str:
+        completion = self.client.beta.chat.completions.parse(
+            model=self.parse_model,
+            messages=self._compress_messages(messages),
+            temperature=0,
+            response_format=response_format
+        )
+        return completion.choices[0].message.parsed
+
+    def complete(self, messages: List[Dict[str, str]], tools: List[Tool] = None) -> str:
+        completion = self.client.chat.completions.create(
+            model=self.completion_model,
+            messages=self._compress_messages(messages),
+            temperature=0,
+            tools=[tool.dump() for tool in tools] if tools else None
+        )
+        if completion.choices[0].finish_reason == "tool_calls":
+            return completion.choices[0].message.tool_calls
+        else:
+            return completion.choices[0].message.content
+
+
+    def force_tool(self, messages: List[Dict[str, str]], tools: List[Tool]) -> List:
+        completion = self.client.chat.completions.create(
+            model=self.tool_model,
+            messages=self._compress_messages(messages),
+            tools=[tool.dump() for tool in tools],
+            temperature=0,
+            tool_choice="required",  # forces tool call
+        )
+        fcts = []
+        for tool_call in completion.choices[0].message.tool_calls:
+            fcts.append({"name": tool_call.function.name, "arguments": tool_call.function.arguments})
+        return fcts
+
+
+class OllamaCompletion(OpenAICompletion):
+
+    def __init__(self, model, url=None, **args):
+        super().__init__(default_model=model, **args)
+        self.client = OpenAI(
+            base_url=url or "http://127.0.0.1:11434/v1",
+            api_key="ollama")
+        
+    def complete(self, messages: List[Dict[str, str]], tools: List[Tool] = None) -> str:
+        if tools is not None:
+            raise NotImplemented
+        return super().complete(messages)
+
+    def parse(self, messages: List[Dict[str, str]], response_format: Type[BaseModel] = None, ) -> str:
+        raise NotImplementedError("not implemented")
+
+    def force_tool(self, messages: List[Dict[str, str]], tools: List[Tool]) -> List:
+        raise NotImplementedError("not implemented")
---
+++ src/gen_ai_blocks/converser.py
@@ -0,0 +1,156 @@
+from typing import Dict, List, Any, Type, Optional
+from pydantic import BaseModel
+from core_llm.completion import Completion, OllamaCompletion
+import json
+
+
+
+class Message(BaseModel):
+    role: str  # system, user, assistant
+    content: str
+
+    def __str__(self):
+        return f"{self.role}: {self.content}"
+
+    def out(self):
+        return {"role": self.role, "content": self.content}
+
+
+class UserMessage(Message):
+    def __init__(self, content):
+        super().__init__(role="user", content=content)
+
+
+class AssistantMessage(Message):
+    def __init__(self, content):
+        super().__init__(role="assistant", content=content)
+
+class Actor:
+    name: str = "Actor"
+
+    def run(self, *args):
+        pass
+
+    def tool(self):
+        pass
+
+
+class SystemMessage(Message):
+    def __init__(self, content):
+        super().__init__(role="system", content=content)
+
+
+class Converser:
+    examples: Dict[str, str]
+    messages: List[Message]
+    completion: Completion
+    persist: str
+
+    def __init__(self,
+                 completion,
+                 completion_options=None,
+                 instruction=None,
+                 persist=None,
+                 examples: Dict[str, str] = None,
+                 actors: List[Actor] = None):
+        self.completion = completion
+        self.persist = persist
+        self.messages = []
+        if instruction is not None:
+            self._add_message(SystemMessage(instruction))
+        if examples is not None:
+            self.examples = examples
+        if actors is not None:
+            self.actors = actors
+
+    def reset(self):
+        if len(self.messages) > 0:
+            inst = self.messages[0]
+            self.messages = []
+            self._add_message(inst)
+
+    def set_instruction(self, instruction: str):
+        # only allow if there are no instruction set - useful for multi step instantiation
+        if len(self.messages) < 1:
+            self._add_message(SystemMessage(instruction))
+
+    def _add_message(self, msg: Message):
+        self.messages.append(msg)
+        if self.persist is not None:
+            self._save(self.persist)
+
+    def _format_messages(self) -> List[Dict[str, str]]:
+        return [msg.out() for msg in self.messages]
+
+    def response(self, str: str, response_obj: Type[BaseModel] = None, ) -> str:
+        self._add_message(UserMessage(content=str))
+        if response_obj is not None:
+            resp = self.completion.parse(
+                messages=self._format_messages(),
+                response_format=response_obj,
+            )
+            self._add_message(AssistantMessage(content=resp.json()))
+
+        else:
+            resp = self.completion.complete(
+                messages=self._format_messages()
+            )
+            self._add_message(AssistantMessage(content=resp))
+
+        return resp
+
+    def tool(self, str: str) -> tuple[Actor, Any] | None:
+        self._add_message(UserMessage(content=str))
+        fcts = self.completion.force_tool(
+            messages=self._format_messages(),
+            tools=[actor.tool() for actor in self.actors])
+
+        self._add_message(AssistantMessage(content=fcts))
+        for actor in self.actors:
+            for fct in fcts:
+                if fct["name"] == actor.name:
+                    return actor, fct["arguments"]
+        return None
+
+    def last_message(self):
+        if len(self.messages) > 0:
+            return self.messages[-1].content
+        else:
+            return None
+
+    def dump(self, fp):
+        with open(fp, 'w') as f:
+            f.write("\n".join([str(m) for m in self.messages]))
+
+    def _save(self, fp):
+        with open(fp, 'w') as f:
+            json.dump([message.dict() for message in self.messages], f, indent=4)
+
+    @staticmethod
+    def load_messages(fp) -> List[Message]:
+        with open(fp, 'r') as f:
+            data = json.load(f)
+            return [Message(**m) for m in data]
+
+
+# # Example: requires ollama llama3.1 running locally
+# from pydantic import Field
+# class RainbowColors(BaseModel):
+#     """colors of the rainbow"""
+#     colors: Optional[List[str]] = None
+#
+# ollama = OllamaCompletion(model="llama3.1", default_temperature=0.0)
+# ollama_converser = Converser(ollama, instruction="you are a helpful assistant")
+# try:
+#     ollama_converser.response("colors of the rainbow", RainbowColors)
+# except NotImplementedError:
+#     print("ollama does not implement parse!")
+# # raises a NotImplemented error because ollama does not support parse method
+# # instead do this:
+# formatter = utility.JSONFormatter(ollama)
+# colors_raw = ollama_converser.response("colors of the rainbow")
+# print(colors_raw)
+# colors_json = formatter.convert(RainbowColors, colors_raw)
+# print(colors_json)
+
+
